8f535c0e30bafdc268de04dba5f543fb75a2d259
chrispmak <ponyboy712@gmail.com>
2016-10-01 17:25:32 -0400

    added production services (actuater)

--- src/main/java/config/App.java
+++ src/main/java/config/App.java
@@ -15,14 +15,6 @@ import java.util.Arrays;
 public class App {
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(App.class, args);
-
-        System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-        String[] beanNames = ctx.getBeanDefinitionNames();
-        Arrays.sort(beanNames);
-        for (String beanName : beanNames) {
-            System.out.println(beanName);
-        }
     }
 
 }
